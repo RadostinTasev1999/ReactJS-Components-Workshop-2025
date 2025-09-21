2aac0dbcac581060bbe32d176a80de83d481c23c
RadostinTasev1999 <radostintasev22@yahoo.com>
2025-09-21 10:48:04 +0300

    Update EditUser.jsx

--- client/src/components/EditUser.jsx
+++ client/src/components/EditUser.jsx
@@ -1,10 +1,12 @@
 export default function UserEdit(
     {
         closeEditForm,
-        // userId,
+        user,
         editUser
     }
 ) {
+
+    console.log('user to edit is:',user)
     return (
         <>
             <div className="overlay">
@@ -28,14 +30,14 @@ export default function UserEdit(
                                     <label htmlFor="firstName">First name</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-user"></i></span>
-                                        <input id="firstName" name="firstName" type="text" />
+                                        <input id="firstName" name="firstName" type="text" defaultValue={user.firstName} />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="lastName">Last name</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-user"></i></span>
-                                        <input id="lastName" name="lastName" type="text" />
+                                        <input id="lastName" name="lastName" type="text" defaultValue={user.lastName} />
                                     </div>
                                 </div>
                             </div>
@@ -45,14 +47,14 @@ export default function UserEdit(
                                     <label htmlFor="email">Email</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-envelope"></i></span>
-                                        <input id="email" name="email" type="text" />
+                                        <input id="email" name="email" type="text" defaultValue={user.email} />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="phoneNumber">Phone number</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-phone"></i></span>
-                                        <input id="phoneNumber" name="phoneNumber" type="text" />
+                                        <input id="phoneNumber" name="phoneNumber" type="text" defaultValue={user.phoneNumber} />
                                     </div>
                                 </div>
                             </div>
@@ -61,7 +63,7 @@ export default function UserEdit(
                                 <label htmlFor="imageUrl">Image Url</label>
                                 <div className="input-wrapper">
                                     <span><i className="fa-solid fa-image"></i></span>
-                                    <input id="imageUrl" name="imageUrl" type="text" />
+                                    <input id="imageUrl" name="imageUrl" type="text" defaultValue={user.imageUrl} />
                                 </div>
                             </div>
 
@@ -70,14 +72,14 @@ export default function UserEdit(
                                     <label htmlFor="country">Country</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-map"></i></span>
-                                        <input id="country" name="country" type="text" />
+                                        <input id="country" name="country" type="text" defaultValue={user.address.country} />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="city">City</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-city"></i></span>
-                                        <input id="city" name="city" type="text" />
+                                        <input id="city" name="city" type="text" defaultValue={user.address.city} />
                                     </div>
                                 </div>
                             </div>
@@ -87,14 +89,14 @@ export default function UserEdit(
                                     <label htmlFor="street">Street</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-map"></i></span>
-                                        <input id="street" name="street" type="text" />
+                                        <input id="street" name="street" type="text" defaultValue={user.address.street} />
                                     </div>
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="streetNumber">Street number</label>
                                     <div className="input-wrapper">
                                         <span><i className="fa-solid fa-house-chimney"></i></span>
-                                        <input id="streetNumber" name="streetNumber" type="text" />
+                                        <input id="streetNumber" name="streetNumber" type="text" defaultValue={user.address.streetNumber} />
                                     </div>
                                 </div>
                             </div>
